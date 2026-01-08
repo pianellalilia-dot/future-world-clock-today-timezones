53f2744db0d8d68ee1821c91e071f81a0fada0f1
pianellalilia-dot <pianella.lilia@gmail.com>
2026-01-08 19:21:38 +0100

    Used display grid to make the time clock looks nicer and responsive

--- src/java.js
+++ src/java.js
@@ -44,14 +44,15 @@ function showCity() {
     let selectedCityName = selectedCityTimezone.split("/")[1];
     let cityList = document.querySelector("#clock-worlds");
     cityList.innerHTML += `<div class="each-city" id="each-city">
-          <div class="each-city-remove-button"><button>Remove <br />City</button></div>
+          <div class="each-city-remove-button" title="Remove City"><button> - </button></div>
+         <div class="grid">
           <div class="each-city-central-box">
             <div class="each-city-name">${selectedCityName}</div>
             <div class="each-city-timezone">${selectedCityTimezone}</div>
             <div class="each-city-date" id="each-city-date">${selectedCityDate}</div>
           </div>
           <div class="each-city-time" id="each-city-time">${selectedCityTime}</div>
-        </div>`;
+        </div></div>`;
   }
 }
 
